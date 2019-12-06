027b8b858c587f9a2067b77ffe532986c2ac9b09
darpan17 <54431407+darpan17@users.noreply.github.com>
2019-12-06 12:34:15 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,6 +1,7 @@
 // implementation of union-find algorithm (DSU)
+// union_(int u,int v) returns true iff nodes u & v are connected
 
-int root[200005],rank[200005];
+int root[MAXN],ranks[MAXN];
  
 int findroot(int node){
 	if(root[node] == node) return node;
@@ -11,12 +12,12 @@ bool union_(int u,int v){
 	if(findroot(u) == findroot(v)) return false;
 	u = findroot(u);
 	v = findroot(v);
-	if(rank[u] > rank[v]){
+	if(ranks[u] > ranks[v]){
 		root[v] = u;
-		rank[u] += rank[v];
+		ranks[u] += ranks[v];
 	}else{
 		root[u] = v;
-		rank[v] += rank[u];
+		ranks[v] += ranks[u];
 	}
 	return true;
 }
