3d0bd2a956df8eece711ad6231c850c3a186fa78
darpan17 <54431407+darpan17@users.noreply.github.com>
2019-12-07 10:44:44 +0530

    Create BInaryIndexedTree.cpp

---
+++ BInaryIndexedTree.cpp
@@ -0,0 +1,21 @@
+// implementation of BIT (also called Fenwick tree)
+// array is assumed to be 1-indexed
+// int may be changed to long long if required
+
+int fenwick[100005],n;
+
+void update(int idx,int val){
+	while(idx <= n){
+		fenwick[idx] += val;
+		idx += (idx&(-idx));
+	}
+}
+
+int query(int idx){
+	int res = 0;
+	while(idx > 0){
+		res += fenwick[idx];
+		idx -= (idx&(-idx)); 
+	}
+	return res;
+}
