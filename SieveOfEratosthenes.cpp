69f7a22ed3fc8101ee74202b733c916fd01218c2
darpan17 <54431407+darpan17@users.noreply.github.com>
2019-12-09 17:33:18 +0530

    Create SieveOfEratosthenes.cpp

---
+++ SieveOfEratosthenes.cpp
@@ -0,0 +1,14 @@
+// implementation of sieve of eratosthenes to find the primes within a given range
+// all prime[i] (1<=i<=MAXN) is initialized to true before function call 
+
+bool prime[MAXN];
+
+void sieve(int n){
+	for(int i = 2; i <= n; i++){
+		if(prime[i]){
+			for(int j = i*2; j <= n; j+=i){
+				prime[j] = false;
+			}
+		}
+	}
+}
