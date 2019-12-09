1eaaa10b6d30115971d05098364c9168d3cb971f
darpan17 <54431407+darpan17@users.noreply.github.com>
2019-12-09 23:28:39 +0530

    Create BellmanFord.cpp

---
+++ BellmanFord.cpp
@@ -0,0 +1,20 @@
+// bellman ford algorithm to find single source shortest path in a graph
+// the graph is assumed to be directed and is stored in the form of a vector of edges
+// complexity: O(|V|*|E|) 
+
+ll dist[MAXN];
+
+void bellman_ford(int src,int n, vector<pair<ll,pair<int,int>>> edges){
+	for(int i = 0; i < n; i++) dist[i] = INF;
+	dist[src] = 0;
+	for(int i = 0; i < n-1; i++){
+		for(auto edge:edges){
+			int start = edge.second.first;
+			int end = edge.second.second;
+			ll wt = edge.first;
+			if(dist[end] > dist[start]+wt){
+				dist[end] = dist[start]+wt;
+			}
+		}
+	}
+}
