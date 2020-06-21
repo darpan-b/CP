af5ad9e49058caad5505342847c6c8ba7579a0dc
winxtron <54431407+winxtron@users.noreply.github.com>
2020-06-21 13:09:16 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,22 +1,17 @@
 #include <bits/stdc++.h>
 
-#define ll long long int
-#define ld long double
+#define F0R(i,n) for(auto i = 0; i < (n); i++)
+#define FOR(i,a,b) for(auto i = (a); i <= (b); i++)
+#define ROF(i,a,b) for(auto i = (a); i >= (b); i--)
+#define ll long long
 #define pb push_back
-#define mp make_pair
-#define pii pair<int,int>
-#define pll pair<ll,ll>
 #define F first
 #define S second
-#define all(x) (x).begin(),(x).end()
-#define SZ(x) ((int)(x).size())
-#define MS(x,a) memset((x),(a),sizeof(x)) 
-#define F0R(i,n) for(auto (i) = 0; (i) < (n); (i)++)
-#define FOR(i,a,b) for(auto (i) = (a); (i) <= (b); (i)++)
-#define ROF(i,a,b) for(auto (i) = (a); (i) >= (b); (i)--)
+#define vi vector<int>
 
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-}	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+}
