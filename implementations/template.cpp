ead6628adf1e80e0563bdbbb07280adaf773c396
winxtron <54431407+winxtron@users.noreply.github.com>
2020-07-05 00:10:38 +0530

    Create template.cpp

---
+++ implementations/template.cpp
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+
+#define F0R(i,n) for(auto i = 0; i < (n); i++)
+#define FOR(i,a,b) for(auto i = (a); i <= (b); i++)
+#define ROF(i,a,b) for(auto i = (a); i >= (b); i--)
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define vi vector<int>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+}
