da40c093fb1c610df3ed27042145840188cc4e45
winxtron <54431407+winxtron@users.noreply.github.com>
2020-07-28 09:57:38 +0530

    Update segment tree.cpp

--- implementations/segment tree.cpp
+++ implementations/segment tree.cpp
@@ -1,39 +1,34 @@
-
-// segment tree for range update and point query
-// this code finds sum of elements in a given change
-// lazy propagation will be uploaded in a separate file
-
-int seg[4*MAXN];
-
-void build(int node,int start,int end,int a[]){
-	if(start == end){
-		seg[node] = a[start];
-	}else{
-		int mid = start+end>>1;
-		build(node*2+1,start,mid,a);
-		build(node*2+2,mid+1,end,a);
-		seg[node] = seg[node*2+1]+seg[node*2+2];
+struct SegTree {
+	ll seg[4*MAXN];
+	ll combine(ll a, ll b) {
+		return a+b;
 	}
-}
-
-void update(int node,int start,int end,int idx,int val){
-	if(idx < start || idx > end) return;
-	if(start == end){
-		seg[node] = val;
-	}else{
-		int mid = start+end>>1;
-		update(node*2+1,start,mid,idx,val);
-		update(node*2+2,mid+1,end,idx,val);
-		seg[node] = seg[2*node+1]+seg[2*node+2];
-	}
-}
-
-int query(int node,int start,int end,int qstart,int qend){
-	if(start > qend || end < qstart) return 0;
-	if(start >= qstart && end <= qend) return seg[node];
-	int res = 0;
-	int mid = start+end>>1;
-	res += query(node*2+1,start,mid,qstart,qend);
-	res += query(node*2+2,mid+1,end,qstart,qend); 
-	return res;
-}
+ 	void build(int node, int ss, int se, ll* a) {
+		if(ss > se) return;
+		if(ss == se) {
+			seg[node] = a[ss]; return;
+		}
+		int mid = ss+se >> 1;
+		build(node*2+1,ss,mid,a);
+		build(node*2+2,mid+1,se,a);
+		seg[node] = combine(seg[node*2+1],seg[node*2+2]);
+ 	}
+ 	void update(int node, int ss, int se, int idx, ll val) {
+ 		if(ss > se || idx > se || idx < ss) return;
+ 		if(ss == se && se == idx) {
+ 			seg[node] = val; return;
+ 		}
+ 		int mid = ss+se >> 1;
+ 		update(node*2+1,ss,mid,idx,val);
+ 		update(node*2+2,mid+1,se,idx,val);
+ 		seg[node] = combine(seg[node*2+1],seg[node*2+2]);
+ 	}
+ 	ll query(int node, int ss, int se, int qs, int qe) {
+ 		if(ss > se || qs > se || qe < ss) return 0;
+ 		if(ss >= qs && se <= qe) return seg[node];
+ 		int mid = ss+se >> 1;
+ 		ll q1 = query(node*2+1,ss,mid,qs,qe);
+ 		ll q2 = query(node*2+2,mid+1,se,qs,qe);
+ 		return q1+q2;
+ 	}
+};
