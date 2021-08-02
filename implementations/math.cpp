5b3506c8427ad5daa4be3b2dc02995bf86150eae
winxtron <54431407+winxtron@users.noreply.github.com>
2021-08-03 01:42:27 +0530

    Update math.cpp

--- implementations/math.cpp
+++ implementations/math.cpp
@@ -1,36 +1,95 @@
-#define ll long long 
+namespace Math
+{
+    
+const long long MOD = 1e9+7;
+const int SIZE = 1e6+5;
 
-namespace Math {
-	const ll mod = 1e9+7;
-	const int SIZE = 1e6+5; 
-	template <typename T> T gcd(T a, T b) { if(a > b) swap(a,b); return (a > 0 ? __gcd(a,b) : b); }
-	template <typename T> T lcm(T a, T b) { return (a*b)/gcd(a,b); }
- 
-	ll add(ll a, ll b, ll p = mod) { ll res = a+b; if(res >= p) res -= p; return res; }
-	ll sub(ll a, ll b, ll p = mod) { ll res = a-b; if(res < 0) res += p; return res; }
-	ll mult(ll a, ll b, ll p = mod) { ll res = a*b; res %= p; return res; }
-	ll fxp(ll a, ll b, ll p = mod) {
-		if(b == 0) return 1;
-		ll res = fxp(a,b/2,p); res %= p;
-		res *= res; res %= p; if(b&1) res *= a; res %= p;
-		return res;
-	}
-	template <typename T> T extendedEuclid(T a, T b, T& x, T& y) {
-		if(b == 0) { x = 1; y = 0; return a; }
-		ll x1, y1; ll g = extendedEuclid(b,a%b,x1,y1); x = y1; y = x1-y1*(a/b);
-		return g;		
-	}
-	template <typename T> T modinv(T x, T p = mod) {
-		T x1,y1,z = 0; T g = extendedEuclid(x,p,x1,y1); 
-		return (g > 1 ? -1: sub(x1,z,p));
-	}
-	ll fact[SIZE],ifact[SIZE];
-	void precompute(ll p = mod) {
-		fact[0] = 1;
-		for(int i = 1; i <= SIZE; i++) fact[i] = mult(fact[i-1],i,p);
-		ifact[SIZE-1] = modinv(fact[SIZE-1],p);
-		for(int i = SIZE-2; i >= 0; i--) ifact[i] = mult(ifact[i+1],i+1,p);
-	}	
-	ll nCr(ll n, ll r, ll p = mod) { return mult(mult(ifact[r],ifact[n-r],p),fact[n],p); }
-} 
+// GCD function which basically uses std::__gcd() but returns gcd(0,0) = 0
+template<typename T> T gcd(T a,T b)
+{
+    if(a > b) swap(a,b);
+    return (a > 0? __gcd(a,b):b);
+}
+template<typename T> T lcm(T a,T b)
+{
+    return (a*b)/gcd(a,b);
+}
+
+
+inline long long add(long long a,long long b)
+{
+    long long res = a+b;
+    while(res >= MOD) res -= MOD;
+    return res;
+}
+inline long long sub(long long a,long long b)
+{
+    long long res = a-b;
+    while(res < 0) res += MOD;
+    return res;
+}
+inline long long mult(long long a,long long b)
+{
+    return (a*b)%MOD;
+}
+
+long long fxp(long long x,long long p)
+{
+    if(p == 0) return 1LL;
+    long long res = fxp(x,p/2);
+    res *= res;
+    res %= MOD;
+    if(p & 1) res *= x;
+    res %= MOD;
+    return res;
+}
+
+// Say ax+by = gcd(a,b)
+// Given a,b, extended euclid calculates the value of gcd(a,b) while simultaneously
+// finding out the coefficients for a and b, that is, x and y respectively.
+long long extendedEuclid(long long a,long long b,long long& x,long long& y)
+{
+    if(b == 0){
+        x = 1;
+        y = 0;
+        return a;
+    }
+    long long x1,y1;
+    long long g = extendedEuclid(b,a%b,x1,y1);
+    x = y1;
+    y = x1-y1*(a/b);
+    return g;
+}
+
+// calculates the modular inverse of x
+long long modinv(long long x)
+{
+    long long x1,y1,z = 0;
+    long long g = extendedEuclid(x,MOD,x1,y1);
+    return (g > 1 ? -1:sub(x1,z));
+}
+
+long long factorial[SIZE];
+long long inverseFactorial[SIZE];
+
+// ALWAYS INITIALIZE precompute() before using any functions in this namespace
+void precompute()
+{
+    factorial[0] = 1;
+    for(int i = 1; i < SIZE; i++){
+        factorial[i] = mult(factorial[i-1],i);
+    }
+    inverseFactorial[SIZE-1] = modinv(factorial[SIZE-1]);
+    for(int i = SIZE-2; i >= 0; i--){
+        inverseFactorial[i] = mult(inverseFactorial[i+1],i+1);
+    }
+}
+
+long long nCr(long long n,long long r)
+{
+    return mult(mult(inverseFactorial[r],inverseFactorial[n-r]),factorial[n]);
+}
+
+} // namespace Math
+
 using namespace Math;
