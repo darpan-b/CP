0d84019c7ee2a14a90f634bf36f7ac466d25a2b1
winxtron <54431407+winxtron@users.noreply.github.com>
2021-09-05 14:10:09 +0530

    Update fenwick tree.cpp

--- implementations/fenwick tree.cpp
+++ implementations/fenwick tree.cpp
@@ -1,62 +1,21 @@
-/*
- * Fenwick tree / Binary indexed tree
- * range query, point update, combine function: addition
- */
-template<typename T>
-struct FenwickTree
-{
-	int n;
-	vector<T> fenwick;
-	FenwickTree()
-	{
-		fenwick.clear();
-	}
-	FenwickTree(int _n)
-	{
-		n=_n;
-		fenwick.assign(n+1,0);
-	}
-	void build(vector<T>& a)
-	{
-		for(int i=1;i<=n;i++){
-			fenwick[i]+=a[i-1];
-			if(i+(i&-i)<=n) fenwick[i+(i&-i)]+=fenwick[i];
-		}
-	}
-	/*
-	 * take note that it DOES NOT change idx to val
-	 * it ADDS val to element at index idx
-	 */
-	void update(int idx,T val)
-	{
-		for(int i=idx+1;i<=n;i+=(i&-i)) fenwick[idx]+=val;
-	}
-	T query(int idx)
-	{
-		T res=0;
-		for(int i=idx+1;i>=1;i-=(i&-1)) res+=fenwick[i];
-		return res;
-	}
-	T query(int l,int r)
-	{
-		return query(r)-query(l-1);
-	}
-};
+#include <bits/stdc++.h>
+using namespace std;
 
+template<typename T>
 struct FenwickTree{
     int n;
-    vector<long long> tree;
+    vector<T> tree;
     FenwickTree() : n(),tree() {}
     FenwickTree(int _n) : n(_n),tree(n+5,0) {}
     
-    void update(int idx,long long val){
+    void update(int idx,T val){
         idx++;
         while(idx<=n){
             tree[idx]+=val;
             idx+=(idx&(-idx));
         }
     }
-    long long query(int idx){
+    T query(int idx){
         idx++;
         long long res=0;
         while(idx>0){
@@ -65,7 +24,7 @@ struct FenwickTree{
         }
         return res;
     }
-    long long query(int l,int r){
+    T query(int l,int r){
         return query(r)-query(l-1);
     }
 };
