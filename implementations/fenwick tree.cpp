25f3d0382983ab292e99c7735dd7933212be5e15
winxtron <54431407+winxtron@users.noreply.github.com>
2021-09-08 00:39:34 +0530

    Update fenwick tree.cpp

--- implementations/fenwick tree.cpp
+++ implementations/fenwick tree.cpp
@@ -1,27 +1,27 @@
 template <typename T>
 struct FenwickTree {
-	int n;
-	vector <T> tree;
-	FenwickTree(): n(), tree() {}
-	FenwickTree(int _n): n(_n), tree(n + 5, 0) {}
+    int n;
+    vector <T> tree;
+    FenwickTree(): n(), tree() {}
+    FenwickTree(int _n): n(_n), tree(n + 5, 0) {}
 
-	void update(int idx, T val) {
-		idx++;
-		while (idx <= n) {
-			tree[idx] += val;
-			idx += (idx & (-idx));
-		}
-	}
-	T query(int idx) {
-		idx++;
-		long long res = 0;
-		while (idx > 0) {
-			res += tree[idx];
-			idx -= (idx & (-idx));
-		}
-		return res;
-	}
-	T query(int l, int r) {
-		return query(r) - query(l - 1);
-	}
+    void update(int idx, T val) {
+        idx++;
+        while (idx <= n) {
+            tree[idx] += val;
+            idx += (idx & (-idx));
+        }
+    }
+    T query(int idx) {
+        idx++;
+        long long res = 0;
+        while (idx > 0) {
+            res += tree[idx];
+            idx -= (idx & (-idx));
+        }
+        return res;
+    }
+    T query(int l, int r) {
+        return query(r) - query(l - 1);
+    }
 };
