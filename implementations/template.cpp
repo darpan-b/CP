79d099b89714cd5778095334343afab2c2cc181f
winxtron <54431407+winxtron@users.noreply.github.com>
2021-09-08 09:52:25 +0530

    Update template.cpp

--- implementations/template.cpp
+++ implementations/template.cpp
@@ -1,25 +1,28 @@
-#ifdef LOCAL
-	#include "debugger.h"
-	#define dbg(...) cerr << "LINE(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", debug(__VA_ARGS__)
-#else
-	#define dbg(...) 5
-#endif
 #include <bits/stdc++.h>
+
 using namespace std;
+
+#ifdef LOCAL
+    #include "debugger.h"
+    #define dbg(...) cerr << "LINE(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", debug(__VA_ARGS__)
+#else
+    #define dbg(...) 5
+#endif
 
 #define ll long long
 #define pb push_back
-#define mp make_pair
-#define F first
-#define S second
+#define ff first
+#define ss second
 
 void solve() {
-	
+
 }
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(0);
-	int test; cin >> test;
-	while(test--) solve();
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int test;
+    cin >> test;
+    while (test--) solve();
+    return 0;
 }
