939e65bf3db72dddfbb1d9ba73fdef4d489233d5
winxtron <54431407+winxtron@users.noreply.github.com>
2021-11-01 08:58:01 +0530

    c++ template for programming competitions

--- implementations/template.cpp
+++ implementations/template.cpp
@@ -1,28 +1,23 @@
 #include <bits/stdc++.h>
-
-using namespace std;
-
 #ifdef LOCAL
 #include "debugger.h"
-#define dbg(...) cerr << "LINE(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", debug(__VA_ARGS__)
+#define dbg(...) cerr << "LINE(" << __LINE__<< ") -> [" << #__VA_ARGS__<< "]: [", debug(__VA_ARGS__)
 #else
 #define dbg(...) 5
+#define endl '\n'
 #endif
+using namespace std;
 
-#define ll long long
-#define pb push_back
-#define ff first
-#define ss second
-
-void solve() {
-
+void solve()
+{
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    int test;
-    cin >> test;
-    while (test--) solve();
+    cin.tie(nullptr);
+    int testcases; cin >> testcases;
+    while(testcases--) solve();
     return 0;
 }
+
