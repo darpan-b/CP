5f0e089bb6555deb8d586f37e660f522ba9e929d
winxtron <54431407+winxtron@users.noreply.github.com>
2021-11-08 21:31:03 +0530

    Update dsu.cpp

--- implementations/dsu.cpp
+++ implementations/dsu.cpp
@@ -1,10 +1,14 @@
+//NOT TESTED
+
 struct DSU {
     vector<int> root;
     vector<int> ranks;
+    
     DSU() {
         root.clear();
         ranks.clear();
     }
+    
     DSU(int _n) {
         root.assign(_n, 0);
         ranks.assign(_n, 0);
@@ -13,21 +17,22 @@ struct DSU {
             ranks[i] = 1;
         }
     }
+    
     int findroot(int node) {
         if (root[node] == node) return node;
         else return root[node] = findroot(root[node]);
     }
+    
     bool sameComp(int u, int v) {
         u = findroot(u);
         v = findroot(v);
         return u == v;
     }
+    
     bool unite(int u, int v) {
         u = findroot(u);
         v = findroot(v);
-        if (u == v) {
-            return false;
-        }
+        if (u == v) return false;
         if (ranks[u] > ranks[v]) {
             ranks[u] += ranks[v];
             root[v] = u;
