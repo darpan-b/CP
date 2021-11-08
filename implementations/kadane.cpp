34f80db95af141e7e013cc7ca2f0b4a658311a62
winxtron <54431407+winxtron@users.noreply.github.com>
2021-11-08 21:45:30 +0530

    Update kadane.cpp

--- implementations/kadane.cpp
+++ implementations/kadane.cpp
@@ -1,9 +1,14 @@
-ll kadane(ll a[],int n){
-	ll maxsum = -1e18,currsum = 0;
-	for(int i = 0; i < n; i++){
-		currsum += a[i];
-		maxsum = max(maxsum,currsum);
-		currsum = max(currsum,0);
-	}
-	return maxsum;
+//NOT TESTED
+
+// Kadane's algorithm for maximum sum subarray.
+
+long long kadane(vector<long long>& a) {
+    long long maxsum = -1e18;
+    long long currsum = 0;
+    for (int i = 0; i < n; i++) {
+        currsum += a[i];
+        maxsum = max(maxsum, currsum);
+        currsum = max(currsum, 0LL);
+    }
+    return maxsum;
 }
