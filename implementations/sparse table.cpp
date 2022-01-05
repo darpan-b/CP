af43ddff43b737bb76118b981a3480d82882c101
winxtron <54431407+winxtron@users.noreply.github.com>
2022-01-06 00:08:07 +0530

    Update sparse table.cpp

--- implementations/sparse table.cpp
+++ implementations/sparse table.cpp
@@ -1,40 +1,33 @@
 template<typename T>
-class SparseTable
-{
-    int n,logn;
-    vector<vector<T>> table;
-
+class SparseTable {
 public:
-    SparseTable()
-    {
-        n = logn = 0;
-        table.clear();
-    }
-    SparseTable(int _n)
-    {  
-        n = _n;
-        logn = __lg(n)+1;
-        table.assign(n,vector<T>(logn,0));
-    }
-    T combine(const T& a, const T& b)
-    {
-        // TODO: modify as per problem
-        return min(a,b);
-    }
+  int n, logn;
+  vector<vector<T>> sparse;
 
-    void build(vector<T>& a)
-    {
-        for(int i = 0; i < n; i++) table[i][0] = a[i];
-        for(int i = 1; i <= logn; i++){
-            for(int j = 0; j < n; j++){
-                if(j+(1<<(i-1)) >= n) continue;
-                table[j][i] = combine(table[j][i-1],table[j+(1<<(i-1))][i-1]);
-            }
-        }
+  SparseTable() = default;
+
+  SparseTable(int n) {
+    this->n = n;
+    logn = __lg(n) + 2;
+    sparse.assign(n, vector<T>(logn, 0));
+  }
+
+  inline T combine(const T& lhs, const T& rhs) {
+    return min(lhs, rhs);
+  }
+
+  void build(const vector<T>& a) {
+    for (int i = 0; i < n; i++) sparse[i][0] = a[i];
+    for (int i = 1; i < logn; i++) {
+      for (int j = 0; j < n; j++) {
+        if (j + (1 << (i - 1)) >= n) continue;
+        sparse[j][i] = combine(sparse[j][i - 1], sparse[j + (1 << (i - 1))][i - 1]);
+      }
     }
-    T query(int l,int r)
-    {
-        int logsz = __lg(r-l+1);
-        return combine(table[l][logsz],table[r -(1<<logsz)+1)][logsz]);
-    }
+  }
+
+  T query(int l, int r) {
+    int logsz = __lg(r - l + 1);
+    return combine(sparse[l][logsz], sparse[r - (1 << logsz) + 1][logsz]);
+  }
 };
