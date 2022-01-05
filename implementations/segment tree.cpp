b827bb936997b3b91bd7a06c4e7aa4f2473040f9
winxtron <54431407+winxtron@users.noreply.github.com>
2022-01-06 00:22:09 +0530

    Update segment tree.cpp

--- implementations/segment tree.cpp
+++ implementations/segment tree.cpp
@@ -1,75 +1,48 @@
 template<typename T>
-class SegmentTree
-{
-    int n;
-    vector<T> seg;
-
+class SegmentTree {
 public:
-    SegmentTree()
-    {
-        n = 0;
-        seg.clear();
-    }   
-    SegmentTree(int _n)
-    {
-        n = _n;
-        seg.assign(4*n, 0);
-    }
+  vector<long long> seg;
 
-private:
-    inline T combine(const T& a, const T& b)
-    {
-        // TODO: change as per problem
-        return a+b;        
+  SegmentTree() = default;
+
+  SegmentTree(int n) {
+    seg.assign(4 * n, 0);
+  }
+
+  inline T combine(const T& lhs, const T& rhs) {
+    return lhs + rhs;
+  }
+
+  void build(int node, int ss, int se, const vector<T>& a) {
+    if (ss > se) return;
+    if (ss == se) {
+      seg[node] = a[ss];
+      return;
     }
-    void build(int node,int ss, int se, const vector<T>& a)
-    {
-        if(ss > se) return;
-        if(ss == se){
-            seg[node] = a[ss];
-            return;
-        }
-        int mid = (ss+se)/2;
-        build(node*2+1,ss,mid,a);
-        build(node*2+2,mid+1,se,a);
-        seg[node] = combine(seg[node*2+1], seg[node*2+2]);
+    int mid = (ss + se) / 2;
+    build(node * 2 + 1, ss, mid, a);
+    build(node * 2 + 2, mid + 1, se, a);
+    seg[node] = combine(seg[node * 2 + 1], seg[node * 2 + 2]);
+  }
+
+  void update(int node, int ss, int se, int uidx, const T& val) {
+    if (ss > se || uidx > se || uidx < ss) return;
+    if (ss == se && ss == uidx) {
+      seg[node] = val;
+      return;
     }
-    void update(int node, int ss,int se,int updidx, const T& val)
-    {
-        if(ss > se || updidx > se || updidx < ss) return;
-        if(ss == se){
-            assert(ss == updidx);
-            seg[node] = val;
-            return;
-        }
-        int mid = (ss+se)/2;
-        update(node*2+1,ss,mid,updidx,val);
-        update(node*2+2,mid+1,se,updidx,val);
-        seg[node] = combine(seg[node*2+1],seg[node*2+2]);
-    }
-    T query(int node,int ss,int se, int qs,int qe)
-    {
-        if(ss > se || qs > se || qe < ss){
-            // TODO: change as per problem
-            return 0;
-        }
-        if(ss >= qs && se <= qe) return seg[node];
-        int mid = (ss+se)/2;
-        T q1 = query(node*2+1,ss,mid,qs,qe);
-        T q2 = query(node*2+2,mid+1,se,qs,qe);
-        return combine(q1,q2);
-    }
-public:
-    inline void build(vector<T>& a)
-    {
-        build(0,0,n-1,a);
-    }
-    inline void update(int updidx,T val)
-    {
-        update(0,0,n-1,updidx,val);
-    }
-    inline T query(int qs,int qe)
-    {
-        return query(0,0,n-1,qs,qe);        
-    }
+    int mid = (ss + se) / 2;
+    update(node * 2 + 1, ss, mid, uidx, val);
+    update(node * 2 + 2, mid + 1, se, uidx, val);
+    seg[node] = combine(seg[node * 2 + 1], seg[node * 2 + 2]);
+  }
+
+  T query(int node, int ss, int se, int qs, int qe) {
+    if (ss > se || qs > se || qe < ss) return 0LL;
+    if (ss >= qs && se <= qe) return seg[node];
+    int mid = (ss + se) / 2;
+    long long leftQuery = query(node * 2 + 1, ss, mid, qs, qe);
+    long long rightQuery = query(node * 2 + 2, mid + 1, se, qs, qe);
+    return combine(leftQuery, rightQuery);
+  }
 };
