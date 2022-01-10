a0b5c18ee7dea5bbf36cc433ef7a74858734e8fe
Darpan Bhattacharya <54431407+winxtron@users.noreply.github.com>
2022-01-10 12:39:07 +0530

    10.01.2022 12.39

--- CSES/Dynamic Programming/DiceCombinations.cpp
+++ CSES/Dynamic Programming/DiceCombinations.cpp
@@ -1,29 +1,30 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 #ifdef LOCAL
-
-#include "/home/winxtron/CLionProjects/cp_clion/debugger.h"
-
-#define dbg(...) cerr << "LINE(" << __LINE__<< ") -> [" << #__VA_ARGS__<< "]: [", debug(__VA_ARGS__)
+#include "debugger.h"
 #else
 #define dbg(...) 5
 #endif
 
-using namespace std;
+int main()
+{
+#ifdef LOCAL
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
 
-int main() {
-    /*
-     * f(x)={f(x-1), f(x-2), ... f(x-6)} + 1
-     */
     int n;
     cin >> n;
-    const long long mod=1e9+7;
-    vector<long long> dp(n+10,0);
-    for(int i=1;i<=6;i++) dp[i]=1;
-    for(int i=2;i<=n;i++) {
-        for(int j=1;j<=6;j++) {
-            if(i-j>=1) dp[i] += dp[i-j];
-            dp[i]%=mod;
+    const long long mod = 1e9 + 7;
+    vector<long long> dp(n + 10, 0);
+    for(int i = 1; i <= 6; i++) dp[i] = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = i - 1; j >= max(1, i - 6); j--)
+        {
+            dp[i] += dp[j];
+            dp[i] %= mod;
         }
     }
     cout << dp[n];
