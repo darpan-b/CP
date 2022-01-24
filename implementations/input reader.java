7a5849c1277de0c949709384b6055c673b9fda11
Darpan Bhattacharya <54431407+winxtron@users.noreply.github.com>
2022-01-24 21:45:14 +0530

    Update input reader.java

--- implementations/input reader.java
+++ implementations/input reader.java
@@ -1,21 +1,20 @@
-//NOT TESTED
-
-class Reader {
-    private BufferedReader br;
-    private StringTokenizer zer;
+class InputReader {
+    private BufferedReader bufferedReader;
+    private StringTokenizer stringTokenizer;
     
-    public Reader() {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        zer = null;
+    public InputReader() {
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        stringTokenizer = null;
     }
     
     public String nextLine() throws IOException {
-        return br.readLine();
+        return bufferedReader.readLine();
     }
     
     public String next() throws IOException {
-        while (zer == null || !zer.hasMoreTokens()) zer = new StringTokenizer(br.readLine());
-        return zer.nextToken();
+        while (stringTokenizer == null || !stringTokenizer.hasMoreTokens())
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        return stringTokenizer.nextToken();
     }
     
     public int nextInt() throws IOException {
@@ -31,6 +30,6 @@ class Reader {
     }
     
     public double nextDouble() throws IOException {
-        return Double.nextDouble(next());
+        return Double.parseDouble(next());
     }
 }
