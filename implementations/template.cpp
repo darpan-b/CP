0a4fd494392e7515981f4559eb70d052b6af6c04
Darpan Bhattacharya <54431407+winxtron@users.noreply.github.com>
2022-02-02 01:29:38 +0530

    Update template.cpp

--- implementations/template.cpp
+++ implementations/template.cpp
@@ -1,23 +1,25 @@
 #include <bits/stdc++.h>
+
+using namespace std;
+
 #ifdef LOCAL
 #include "debugger.h"
-#define dbg(...) cerr << "LINE(" << __LINE__<< ") -> [" << #__VA_ARGS__<< "]: [", debug(__VA_ARGS__)
 #else
 #define dbg(...) 5
-#define endl '\n'
 #endif
-using namespace std;
 
 void solve()
 {
+    
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int testcases; cin >> testcases;
-    while(testcases--) solve();
+    int test;
+    cin >> test;
+    while (test--)
+        solve();
     return 0;
 }
-
