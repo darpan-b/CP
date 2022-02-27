f12119728c231496a7cc99643dfa90f4ea3b7180
Darpan Bhattacharya <54431407+winxtron@users.noreply.github.com>
2022-02-27 23:55:55 +0530

    Create MissingNumber.cpp

---
+++ CSES_new/Introductory Problems/MissingNumber.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	vector<int> a(n-1);
+	for (int i=0; i<n-1; i++)
+		scanf("%d", &a[i]);
+	long long sum=0;
+	for(int i=0; i<n-1; i++)
+		sum += a[i];
+	printf("%lld\n", (n*(n+1LL))/2 - sum);
+	return 0;
+}
