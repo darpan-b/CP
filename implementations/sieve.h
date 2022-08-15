d3d8f38d45e96380ee540a81127e48139e8e9ef1
Darpan <54431407+winterflame17@users.noreply.github.com>
2022-08-15 10:37:56 +0530

    Add files via upload

---
+++ implementations/sieve.h
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct sieve
+{
+    vector<bool> is_prime;
+
+    sieve() {}
+
+    sieve(int n)
+    {
+        is_prime.assign(n + 1, true);
+        is_prime[0] = is_prime[1] = false;
+        for (int i = 2; i <= n; i++)
+        {
+            if (!is_prime[i]) continue;
+            for (int j = i + i; j <= n; j += i) is_prime[i] = false;
+        }
+    }
+
+    bool prime(int x)
+    {
+        return sieve[x];
+    }
+};
