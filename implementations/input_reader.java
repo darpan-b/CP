a67324f330cd95bc5bb24d889e04da42d947a7cd
Darpan <54431407+winterflame17@users.noreply.github.com>
2022-08-27 23:22:15 +0530

    Update input_reader.java

--- implementations/input_reader.java
+++ implementations/input_reader.java
@@ -1,44 +1,36 @@
-class InputReader
-{
+class InputReader {
     private BufferedReader bufferedReader;
     private StringTokenizer stringTokenizer;
     
-    public InputReader()
-    {
+    public InputReader() {
         bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         stringTokenizer = null;
     }
     
-    public String nextLine() throws IOException
-    {
+    public String nextLine() throws IOException {
         return bufferedReader.readLine();
     }
     
-    public String next() throws IOException
-    {
+    public String next() throws IOException {
         while (stringTokenizer == null || !stringTokenizer.hasMoreTokens()) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         }
         return stringTokenizer.nextToken();
     }
     
-    public int nextInt() throws IOException
-    {
+    public int nextInt() throws IOException {
         return Integer.parseInt(next());
     }
     
-    public long nextLong() throws IOException
-    {
+    public long nextLong() throws IOException {
         return Long.parseLong(next());
     }
     
-    public float nextFloat() throws IOException
-    {
+    public float nextFloat() throws IOException {
         return Float.parseFloat(next());
     }
     
-    public double nextDouble() throws IOException
-    {
+    public double nextDouble() throws IOException {
         return Double.parseDouble(next());
     }
 }
