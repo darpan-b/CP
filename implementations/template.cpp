5f3a72d92ef36033123d38713c5359cd8ffeba1e
Darpan Bhattacharya <54431407+darpan-b@users.noreply.github.com>
2023-01-06 10:13:54 +0530

    Create template.cpp

---
+++ implementations/template.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "debugger.h"
+#else
+#define dbg(...) 2023
+#endif
+
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pli pair<ll, int>
+#define pil pair<int, ll>
+#define vi vector<int>
+#define vl vector<ll>
+#define vd vector<ld>
+#define F0R(i, n) for (int i = 0; i < (n); i++)
+#define FOR(i, a, b) for (auto i = (a); i <= (b); i++)
+#define ROF(i, a, b) for (auto i = (a); i >= (b); i--)
+#define TRAV(i, a) for (auto& i : a)
+#define all(x) x.begin(), x.end()
+#define ff first
+#define ss second
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int test;
+    cin >> test;
+    while(test--) solve();
+    return 0;
+}
