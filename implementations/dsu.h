86819b3f0cfa278c7deba186a2f5b2aa21d749d1
Darpan Bhattacharya <54431407+darpan-b@users.noreply.github.com>
2023-01-06 10:12:07 +0530

    Update dsu.h

--- implementations/dsu.h
+++ implementations/dsu.h
@@ -1,44 +1,25 @@
-struct disjoint_set_union
-{
-    vector<int> roots, ranks;
-
-    disjoint_set_union()
-    {
+struct DSU {
+    vi roots, ranks;
+    DSU() {
         roots.clear();
         ranks.clear();
     }
-
-    disjoint_set_union(int n)
-    {
+    DSU(int n) {
         roots.assign(n + 1, 0);
         ranks.assign(n + 1, 0);
-        iota(roots.begin(), roots.end(), 0);
+        iota(all(roots), 0);
     }
-
-    int find_root(int u)
-    {
+    int findroot(int u) {
         if (roots[u] == u) return u;
-        return roots[u] = find_root(roots[u]);
+        return roots[u] = findroot(roots[u]);
     }
-
-    bool unite(int u, int v)
-    {
-        u = find_root(u);
-        v = find_root(v);
+    bool unite(int u, int v) {
+        u = findroot(u);
+        v = findroot(v);
         if (u == v) return false;
-        if (ranks[u] > ranks[v])
-        {
-            roots[v] = u;
-            ranks[u] += ranks[v];
-        }
-        else
-        {
-            roots[u] = v;
-            ranks[v] += ranks[u];
-        }
+        if (ranks[v] > ranks[u]) swap(u, v);
+        roots[v] = u;
+        ranks[u] += ranks[v];
         return true;
     }
 };
-
-
-
