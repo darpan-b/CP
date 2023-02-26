7e58d6243770300bf52a5bb6b5c616404f15bdb9
Darpan Bhattacharya <54431407+darpan-b@users.noreply.github.com>
2023-02-26 14:33:52 +0530

    Update floyd warshall.cpp

--- implementations/floyd warshall.cpp
+++ implementations/floyd warshall.cpp
@@ -1,16 +1,10 @@
-//NOT TESTED
-
-// Floyd-Warshall algorithm to find all pairs shortest path.
-// Time complexity: O(N * N * N)
-
 //Initialize dist[i][j] to inf or edge weight for all i, j before function call
-
-void floydWarshall(vector<vector<long long>>& dist) {
-    int n = dist.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < n; k++) {
-                if (dist[j][k] > dist[j][i] + dist[i][k]) {
+void floyd_warshall(vector<vector<ll>>& dist) {
+    int n = (int)dist.size();
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            for(int k = 0; k < n; k++){
+                if(dist[j][k] > dist[j][i] + dist[i][k]){
                     dist[j][k] = dist[j][i] + dist[j][k];
                 }
             }
