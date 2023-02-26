c1a33255a96e30e17da52424c652cc15138d23c9
Darpan Bhattacharya <54431407+darpan-b@users.noreply.github.com>
2023-02-26 14:32:33 +0530

    Update fenwick_tree.h

--- implementations/fenwick_tree.h
+++ implementations/fenwick_tree.h
@@ -1,39 +1,37 @@
 template<typename T>
-struct fenwick_tree
-{
+struct FenwickTree{
     int n;
     vector<T> ftree; // 1-indexed
 
-    fenwick_tree()
-    {
+    FenwickTree(){
         n = 0;
         ftree.clear();
     }
 
-    fenwick_tree(int _n)
-    {
+    FenwickTree(int _n){
         n = _n;
         ftree.assign(n + 5, 0);
     }
 
     /* this just ADDS data to index, not REPLACE it */
-    void update(int pos, T data)
-    {
+    void update(int pos, T data){
         pos++;
-        for (; pos <= n; pos += (pos & (-pos))) ftree[pos] += data;
+        for (; pos <= n; pos += (pos & (-pos))){
+            ftree[pos] += data;
+        }
     }
 
-    T query(int pos)
-    {
+    T query(int pos){
         pos++;
         T res = 0;
-        for (; pos > 0; pos -= (pos & (-pos))) res += ftree[pos];
+        for (; pos > 0; pos -= (pos & (-pos))){
+            res += ftree[pos];
+        }
         return res;
     }
 
     /* returns sum in range [lo, hi] */
-    T query(int lo, int hi)
-    {
+    T query(int lo, int hi){
         return query(hi) - query(lo - 1);
     }
 };
