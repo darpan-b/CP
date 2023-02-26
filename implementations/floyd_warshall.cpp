25311a6e3a562dfb424748c4f5ecffe58654c2b9
Darpan Bhattacharya <54431407+darpan-b@users.noreply.github.com>
2023-02-26 14:34:11 +0530

    Update floyd_warshall.cpp

--- implementations/floyd_warshall.cpp
+++ implementations/floyd_warshall.cpp
@@ -1,5 +1,5 @@
 //Initialize dist[i][j] to inf or edge weight for all i, j before function call
-void floyd_warshall(vector<vector<ll>>& dist) {
+void floyd_warshall(vector<vector<ll>>& dist){
     int n = (int)dist.size();
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
